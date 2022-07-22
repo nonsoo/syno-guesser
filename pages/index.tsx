971fe1dd74afb43df5d9fe747894f1f0f09f1117
Nonso <nonsootoh@gmail.com>
2022-07-22 18:46:14 -0400

    style: updates favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -166,7 +166,24 @@ const Home: NextPage<Props> = ({ data, wordOfDay }) => {
         <title>Clueless Words</title>
         <meta
           name="description"
-          content="Can you use the clueless words (synonyms) to solve the mystery word? You only have 6 tries! Wrong guesses will show up in the guess bucket!"
+          content="Can you use the clueless words (these synonyms) to solve the mystery word? You only have 6 tries! Wrong guesses will show up in the guess bucket!"
+        />
+        <link
+          rel="shortcut icon"
+          href="/favicon/favicon.ico"
+          type="image/x-icon"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
         />
         <link
           rel="icon"
@@ -174,6 +191,7 @@ const Home: NextPage<Props> = ({ data, wordOfDay }) => {
           sizes="192x192"
           href="favicon/android-chrome-192x192.png"
         />
+        <link rel="apple-touch-icon" href="favicon/apple-touch-icon.png" />
       </Head>
 
       <header className={styles.HeaderCon}>
