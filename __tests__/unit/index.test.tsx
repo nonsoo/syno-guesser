bf507078d54d9285ac921e999604d5af455ae339
Nonso <nonsootoh@gmail.com>
2022-08-02 22:24:20 -0400

    chore: adds new packages and adds new tests

--- __tests__/unit/index.test.tsx
+++ __tests__/unit/index.test.tsx
@@ -1,6 +1,18 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+import { mockData } from "../../utils/types/testTypes";
+
 import Home from "../../pages/index";
 
-describe("", () => {});
+describe("Test the application for Functionality", () => {
+  describe("Instruction page", () => {
+    it("should mount the instruction page when page is loaded", () => {
+      render(<Home {...mockData} />);
+
+      const InstructionHeader = screen.getByAltText(/How to play/i);
+
+      expect(InstructionHeader).toBeVisible();
+    });
+  });
+});
