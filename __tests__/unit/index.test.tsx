32a39bc82863d0282315779913687e2754c2cada
Nonso <nonsootoh@gmail.com>
2022-08-08 22:28:36 -0400

    test: fix mouting instruction page test

--- __tests__/unit/index.test.tsx
+++ __tests__/unit/index.test.tsx
@@ -11,7 +11,7 @@ describe("Test the application for Functionality", () => {
     it("should mount the instruction page when page is loaded", () => {
       render(<Home synonyms={stringWords} wordOfDay={word} />);
 
-      const InstructionHeader = screen.getByAltText(/How to play/i);
+      const InstructionHeader = screen.getByText(/How to play/i);
 
       expect(InstructionHeader).toBeVisible();
     });
