3a6d2a58f9a43513dcc4b97b8db5acbad4c7d262
Nonso <nonsootoh@gmail.com>
2022-08-09 23:10:13 -0400

    adds new tests

--- __tests__/unit/index.test.tsx
+++ __tests__/unit/index.test.tsx
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom";
 
 import Home from "../../pages/index";
 
-const stringWords = ["hello", "There"];
+const stringWords = ["hello", "There", "you", "test", "test1"];
 const word: string = "there";
 
 describe("Test the application for Functionality", () => {
@@ -18,6 +18,9 @@ describe("Test the application for Functionality", () => {
   });
 
   describe("testing the game", () => {
+    beforeEach(() => {
+      window.localStorage.clear();
+    });
     it("should allow user to submit a guess", () => {
       render(<Home synonyms={stringWords} wordOfDay={word} />);
       const closeBtn = screen.getByTestId("instruct_Close_btn");
@@ -34,6 +37,38 @@ describe("Test the application for Functionality", () => {
       const guessedWord = screen.getByTestId("GuessedWord");
 
       expect(guessedWord).toBeInTheDocument();
+    });
+
+    it("should show the user that they have won the game", () => {
+      render(<Home synonyms={stringWords} wordOfDay={word} />);
+      const closeBtn = screen.getByTestId("instruct_Close_btn");
+      fireEvent.click(closeBtn);
+
+      const guessInput = screen.getByRole("textbox");
+      const form = screen.getByTestId("formSubmit");
+      fireEvent.change(guessInput, {
+        target: { value: word },
+      });
+
+      fireEvent.submit(form);
+
+      const winScreen = screen.getByTestId("winState");
+
+      expect(winScreen).toBeVisible();
+    });
+
+    it("should show the user a new synonym when the new hint btn is pressed", () => {
+      render(<Home synonyms={stringWords} wordOfDay={word} />);
+      const closeBtn = screen.getByTestId("instruct_Close_btn");
+      fireEvent.click(closeBtn);
+
+      const newHint = screen.getByRole("button", { name: "New Hint" });
+
+      fireEvent.click(newHint);
+
+      const synosLst = screen.getAllByTestId("synos");
+
+      expect(synosLst.length).toEqual(4);
     });
   });
 });
