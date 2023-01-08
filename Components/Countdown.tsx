63e6253363f0d8496b52c8877a6081e1b71ee702
Nonso <nonsootoh@gmail.com>
2023-01-07 21:13:50 -0500

    fix: changes utc string to memo value

--- Components/Countdown.tsx
+++ Components/Countdown.tsx
@@ -5,8 +5,8 @@ import useCountdown from "../utils/hooks/useCountdown";
 import styles from "../styles/wingame.module.css";
 
 const Countdown: FC = () => {
-  const tomorrow = useMemo(() => startOfTomorrow(), []);
-  const [, hours, minutes, seconds] = useCountdown(tomorrow.toUTCString());
+  const tomorrow = useMemo(() => startOfTomorrow().toUTCString(), []);
+  const [, hours, minutes, seconds] = useCountdown(tomorrow);
 
   return (
     <p className={styles.countDown}>
