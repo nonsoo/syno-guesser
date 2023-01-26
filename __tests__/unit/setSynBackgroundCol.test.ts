b6821160a6f527f41aff2509db58d78d633d25d7
Nonso <nonsootoh@gmail.com>
2023-01-25 21:15:21 -0500

    test: adds new test

--- __tests__/unit/setSynBackgroundCol.test.ts
+++ __tests__/unit/setSynBackgroundCol.test.ts
@@ -11,6 +11,16 @@ describe("setSynBackgroundCol", () => {
     );
   });
 
+  it('should return "hsl(111, 32%, 38%)" if myGuess is the secret word', () => {
+    const myGuess = "book";
+    const trgWords = ["hello", "world"];
+    const synonyms = ["hi", "greetings"];
+    const secretWord = "book";
+    expect(setSynBackgroundCol(myGuess, trgWords, synonyms, secretWord)).toBe(
+      "hsl(111, 32%, 38%)"
+    );
+  });
+
   it('should return "hsl(0, 84%, 68%)" if myGuess is not included in trgWords or synonyms', () => {
     const myGuess = "goodbye";
     const trgWords = ["hello", "world"];
