ad78aba593c0a03721c3db2f706c91ef4ca2e319
Nonso <nonsootoh@gmail.com>
2023-03-25 18:35:09 -0400

    test: adds new test for getHint

--- utils/hooks/useGetHint.ts
+++ utils/hooks/useGetHint.ts
@@ -1,5 +1,5 @@
 import { synonyms } from "../types/projectTypes";
-const useGetHint = (hintSet: Set<number>, lenLst: number) => {
+export const useGetHint = (hintSet: Set<number>, lenLst: number) => {
   let randomHint: number = 0;
   let counter: number = 0;
 
@@ -13,14 +13,8 @@ const useGetHint = (hintSet: Set<number>, lenLst: number) => {
   }
 
   return randomHint;
-
-  // make an array of indexs 0 ... lenlst of
-
-  // sort a random list of hints and get the first index and then pop from list
 };
 
 export const newRandomHint = (synonymsLstRandom: synonyms) => {
   return synonymsLstRandom.pop();
 };
-
-export default useGetHint;
