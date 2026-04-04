9f20b88102262d7e7031c4eb1323da030710437b
Nonso <nonsootoh@gmail.com>
2026-04-03 20:58:18 -0400

    refactor: moves logic to server file and cleans up logic

--- utils/hooks/useGetTriggerWords.ts
+++
@@ -1,9 +0,0 @@
-import type { TriggerWord } from "../types/projectTypes";
-
-const useGetTriggerWords = (lst: TriggerWord[]) => {
-  const newArray = lst.map((trgWord) => trgWord.word);
-
-  return newArray;
-};
-
-export default useGetTriggerWords;
