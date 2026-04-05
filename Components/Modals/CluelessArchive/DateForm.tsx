2cbfd09634fec1b900f0789668158f0886637437
Nonso <nonsootoh@gmail.com>
2026-04-05 12:24:01 -0400

    feat: adds random day selector to archive

--- Components/Modals/CluelessArchive/DateForm.tsx
+++ Components/Modals/CluelessArchive/DateForm.tsx
@@ -3,12 +3,13 @@ import { useRouter } from "next/navigation";
 
 import { startTransition, useState } from "react";
 
-import { subDays } from "date-fns";
+import { format, subDays } from "date-fns";
 
 import DateSelector from "@/Components/DateSelector/DateSelector";
 import styles from "@/styles/CluelessArchive.module.css";
 import { BASELINE_DATE } from "@/utils/constants/consts";
 import { CLUELESS_ARCHIVE_MODAL_ID } from "@/utils/constants/id-constants";
+import { getRandomDateByDay } from "@/utils/helpers/dates";
 
 const DateForm = () => {
   const fromDate = new Date(BASELINE_DATE);
@@ -17,7 +18,7 @@ const DateForm = () => {
   const defaultDate = subDays(toDate, 1);
   const [selectedDate, setSelectedDate] = useState<Date>(defaultDate);
 
-  const formattedDate = selectedDate.toISOString().split("T")[0];
+  const formattedDate = format(selectedDate, "yyyy-MM-dd");
 
   const router = useRouter();
 
@@ -30,19 +31,35 @@ const DateForm = () => {
         toDate={toDate}
         disabledRange={{ before: fromDate, after: defaultDate }}
       />
-      <button
-        type="button"
-        className={styles.Modal__submitBtn}
-        popoverTarget={CLUELESS_ARCHIVE_MODAL_ID}
-        popoverTargetAction="hide"
-        onClick={() =>
-          startTransition(() => {
-            router.push(`/archive/${formattedDate}`);
-          })
-        }
-      >
-        Play Archived Game
-      </button>
+      <section className={styles.btnGroups}>
+        <button
+          type="button"
+          className={styles.Modal__submitBtn}
+          popoverTarget={CLUELESS_ARCHIVE_MODAL_ID}
+          popoverTargetAction="hide"
+          onClick={() =>
+            startTransition(() => {
+              router.push(`/archive/${formattedDate}`);
+            })
+          }
+        >
+          Play Archived Game
+        </button>
+        <button
+          type="button"
+          className={styles.Modal__submitBtn}
+          popoverTarget={CLUELESS_ARCHIVE_MODAL_ID}
+          popoverTargetAction="hide"
+          onClick={() =>
+            startTransition(() => {
+              const randomDay = getRandomDateByDay(fromDate, defaultDate);
+              router.push(`/archive/${randomDay}`);
+            })
+          }
+        >
+          Play Random Day
+        </button>
+      </section>
     </form>
   );
 };
